646a4a40c5585ec1a231678a6756d5a7dbf9e6ae
Megha <meghs15sh@gmail.com>
2018-07-17 19:04:14 +0530

    KeywordDrivenScript

---
+++ src/keywordDriven/KeywordDrivenScript.java
@@ -0,0 +1,33 @@
+package keywordDriven;
+
+import java.io.IOException;
+
+public class KeywordDrivenScript
+{
+	public static void main(String[] args) throws IOException, InterruptedException 
+	{
+		String spath = "C:\\Users\\admin\\Desktop\\KeyExcel.xlsx";
+		
+		
+		readExcel.setExcel(spath , "Sheet1");
+		
+		for(int iRow = 1 ; iRow <= 7 ; iRow++)
+		{
+			String sActions = readExcel.getCellData(iRow, 1);
+			
+			if(sActions.equals("openBrowser"))
+			{
+				Action.openBrowser();
+			}
+			else if (sActions.equals("navigate"))
+			{
+				Action.navigate();
+			}
+			else if (sActions.equals("click"))
+			{
+				Action.click();
+			}
+
+		}
+	}
+}
